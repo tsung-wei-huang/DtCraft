8e9d1fa27a2081ae103f597f25b9c5e31dc5eb31
Tsung-Wei Huang <twh760812@gmail.com>
2017-12-01 21:04:34 -0600

    Add files via upload

---
+++ conf/conf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Environmental variables for DTC. Users are free to uncomment and modify the setting.
+
+#export DTC_MASTER_HOST=127.0.0.1;
+#export DTC_AGENT_LISTENER_PORT=9909;
+#export DTC_EXECUTOR_LISTENER_PORT=9910;
+#export DTC_CMSG_LISTENER_LISTENER_PORT=9911;
+#export DTC_WEBUI_PORT=9912;
+
+#export DTC_SOCKET_CONNECTION_TIMEOUT=10;
+#export DTC_FIFO_CONNECTION_TIMEOUT=10;
+#export DTC_SHM_CONNECTION_TIMEOUT=10;
+#export DTC_LOADINFO_PERIODIC=10;
+
+#export DTC_LOG_DIR=""
+#export DTC_PID_DIR=""
