8d4588df8a77df383ac94c9728af54c55b987c9a
twhuang <twh760812@gmail.com>
2018-05-18 11:00:17 -0500

    add demo.cpp

---
+++ app/demo/demo.cpp
@@ -0,0 +1,12 @@
+#include <dtc/dtc.hpp>
+
+int main(int argc, char* argv[]) {
+  
+  std::cout << "This is a demo program\n"
+            << "Use the following instructions to create your app:\n"
+            << "1. create a folder under 'app/', the name will be your app name\n"
+            << "2. run './Makefile.sh' to include your app to our build system\n"
+            << "3. type 'make' to build\n";
+
+  return 0;
+}
